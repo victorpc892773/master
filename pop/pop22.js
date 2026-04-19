a977c71f751ac815141a33712bcff3f68497a6e7
victorpc892773 <victorpc892773@gmail.com>
2026-04-19 11:20:38 -0300

    Create pop22.js

---
+++ pop/pop22.js
@@ -0,0 +1,9 @@
+var POPUP_ATIVO = true;
+(function() {
+    if (!POPUP_ATIVO) return;
+
+    var s = document.createElement('script');
+    s.setAttribute('data-cfasync', 'false');
+    s.src = '//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1167915';
+    document.head.appendChild(s);
+})();
