22665380a16b6c251b03138fb5d66eee08487b6e
victorpc892773 <victorpc892773@gmail.com>
2026-04-19 12:56:32 -0300

    Update pop.js

--- pop/pop.js
+++ pop/pop.js
@@ -4,7 +4,7 @@ var POPUP_ATIVO = true;
     var url = window.location.href;
 
     if (!POPUP_ATIVO) {
-        console.log('%c[POPUP] ❌ Desligado | Página: ' + titulo, 'color: orange; font-weight: bold;');
+        console.log('%c[POPUP - SECUNDÁRIO] ❌ Desligado | Página: ' + titulo, 'color: orange; font-weight: bold;');
         return;
     }
 
@@ -13,11 +13,11 @@ var POPUP_ATIVO = true;
     s.src = '//dcbbwymp1bhlf.cloudfront.net/?wbbcd=1167915';
 
     s.onload = function() {
-        console.log('%c[POPUP] ✅ Carregado com sucesso | Página: ' + titulo + ' | URL: ' + url, 'color: green; font-weight: bold;');
+        console.log('%c[POPUP - SECUNDÁRIO] ✅ Carregado! | Página: ' + titulo + ' | URL: ' + url, 'color: green; font-weight: bold;');
     };
 
     s.onerror = function() {
-        console.log('%c[POPUP] ❌ Erro ao carregar | Página: ' + titulo, 'color: red; font-weight: bold;');
+        console.log('%c[POPUP - SECUNDÁRIO] ❌ Erro ao carregar | Página: ' + titulo, 'color: red; font-weight: bold;');
     };
 
     document.head.appendChild(s);
