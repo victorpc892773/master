96ac91230b31c10146382a318744c51c8ceb4ae6
victorpc892773 <victorpc892773@gmail.com>
2026-04-22 11:58:12 -0300

    Update pop.js

--- pop/pop.js
+++ pop/pop.js
@@ -3,7 +3,7 @@ var ATIVO = false; // false or true
 (function(){
   if(!ATIVO) return;
   var z=['5888865','7514922'],s=document.createElement('script');
-  s.dataset.zone=z[Math.random()*3|0];
+  s.dataset.zone=z[Math.random()*2|0];
   s.src='https://llvpn.com/tag.min.js';
   console.log('[AdZone]',s.dataset.zone);
   (document.body||document.documentElement).appendChild(s);
