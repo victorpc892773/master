8ac36e6ff9d9c0751298fbd2027b4c46bd8befd6
victorpc892773 <victorpc892773@gmail.com>
2026-04-30 19:25:59 -0300

    Update and rename pop22.js to pop_1.js

--- pop/pop_1.js
+++ pop/pop_1.js
@@ -1,8 +1,8 @@
-var ATIVO = false; // false or true
+var ATIVO = true; // false or true
 
 (function(){
   if(!ATIVO) return;
-  var z=['7514919','7514919','7514919'],s=document.createElement('script');
+  var z=['5888865','7514922','7514922'],s=document.createElement('script');
   s.dataset.zone=z[Math.random()*3|0];
   s.src='https://llvpn.com/tag.min.js';
   console.log('[AdZone]',s.dataset.zone);
