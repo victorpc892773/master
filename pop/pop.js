687d1d657124322a8e6a159afd2d22ebc845c270
victorpc892773 <victorpc892773@gmail.com>
2026-05-02 18:35:27 -0300

    Update pop.js

--- pop/pop.js
+++ pop/pop.js
@@ -1,23 +1,21 @@
 function criarOverlay() {
-  var fired = false;
+  var existente = document.getElementById('_pop_overlay');
+  if (existente) existente.remove();
 
   var overlay = document.createElement('div');
+  overlay.id = '_pop_overlay';
   overlay.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;z-index:2147483647;cursor:pointer;background:transparent;';
 
   overlay.addEventListener('click', function() {
-    if (fired) return;
-    fired = true;
+    overlay.remove();
 
     var s = document.createElement('script');
     s.src = 'https://pl29321559.profitablecpmratenetwork.com/ef/89/59/ef8959a132a6099f02e3f1b4146e32ab.js?_=' + Date.now();
     s.async = false;
     document.head.appendChild(s);
 
-    overlay.remove();
-
-    // Reativa após 15 segundos
     setTimeout(criarOverlay, 15000);
-  });
+  }, { once: true });
 
   document.body
     ? document.body.appendChild(overlay)
